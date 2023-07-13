364b6e60fd41b62d1bad04726232a4bb9193d7fc
Tarun kumar <tarunkchandra@gmail.com>
2023-07-13 16:28:46 +0530

    validation and modification don in feedback form

--- src/Components/FeedbackForm/FeedbackForm.jsx
+++ src/Components/FeedbackForm/FeedbackForm.jsx
@@ -4,10 +4,11 @@ import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
 import "./feedbackform.css";
 import { GetConfigureData } from "../../redux/slices/courses/coursesActions";
-import {createFeedback} from "../../redux/slices/ratingCourse/RatingCourseAction";
+import { createFeedback } from "../../redux/slices/ratingCourse/RatingCourseAction";
 
 const FeedbackForm = () => {
   const dispatch = useDispatch();
+
   const [openRights, setOpenRights] = useState(false);
   const [modalIsOpen, setModalIsOpen] = useState(false);
   const [announcementText, setAnnouncementtext] = useState();
@@ -19,17 +20,21 @@ const FeedbackForm = () => {
     (state) => state?.courseState?.announcementList
   );
 
-  const feedbackType = configureData.find(x => x.moduleId == 3)?.configurations;
-  const feedbackCategory = configureData.find(x => x.moduleId == 4)?.configurations;
- 
-  const [formData , setFormData] = useState({
-    id:0,
+  const feedbackType = configureData.find(
+    (x) => x.moduleId == 3
+  )?.configurations;
+  const feedbackCategory = configureData.find(
+    (x) => x.moduleId == 4
+  )?.configurations;
+
+  const [formData, setFormData] = useState({
+    id: 0,
     feedbackType: "",
     feedbackCategory: "",
     descriptions: "",
     userName: "",
     email: "",
-    mobile_Number: "",     
+    mobile_Number: "",
   });
 
   useEffect(() => {
@@ -37,9 +42,25 @@ const FeedbackForm = () => {
   }, []);
 
   const handlefeedbackData = () => {
-    console.log("feedback data",formData);
+    console.log("feedback data", formData);
     dispatch(createFeedback(formData));
-   
+  };
+
+  const submitAfterValidation = (e) => {
+    e.preventDefault();
+    handlefeedbackData();
+  };
+
+  const reset = () => {
+    setFormData({
+      id: 0,
+      feedbackType: "",
+      feedbackCategory: "",
+      descriptions: "",
+      userName: "",
+      email: "",
+      mobile_Number: "",
+    });
   };
 
   return (
@@ -56,153 +77,143 @@ const FeedbackForm = () => {
           </p>
         </div>
         <div className=" top-header p-3 my-3 mx-auto">
-          <Form autoComplete="off" className="">
-            <div className="text-primary text-center mb-4">
-              {/* <bd.icons.Email style={{ fontSize: 50 }} /> */}
+          <Form
+            autoComplete="off"
+            className="was-validated"
+            onSubmit={submitAfterValidation}
+          >
+            <div className="text-primary text-center mb-4"></div>
+
+            <div className="form-floating mb-3 mt-3">
+              <select
+                id="accessType"
+                name="accessType"
+                className="form-control"
+                onChange={(e) =>
+                  setFormData({ ...formData, feedbackType: e.target.value })
+                }
+                required
+              >
+                <option disabled selected value="">
+                  Select Types
+                </option>
+                {feedbackType?.map((ele) => {
+                  return (
+                    <>
+                      <option value={ele?.configurationId}>{ele?.value}</option>
+                    </>
+                  );
+                })}
+              </select>
+              <label for="type">
+                What type of feedback would you like to send?
+              </label>
             </div>
 
-            <div className="col-6-area">
-              <div className="form-group">
-                <select
-                  id="accessType"
-                  name="accessType"
-                  className="form-control"
-                  onChange={(e) =>
-                    setFormData({ ...formData, feedbackType: e.target.value })
-                  }
-                  required
-                >
-                  <option disabled selected value="">
-                    Select Types
-                  </option>
-                  {feedbackType?.map((ele) => {
+            <div className="form-floating mb-3 mt-3">
+              <select
+                id="accessType"
+                name="accessType"
+                className="form-control"
+                onChange={(e) =>
+                  setFormData({ ...formData, feedbackCategory: e.target.value })
+                }
+                required
+              >
+                <option disabled selected value="">
+                  Select Category
+                </option>
 
-
-                    return (
-                      <>
-                        <option value={ele?.configurationId}>
-                          {ele?.value}
-                        </option>
-                      </>
-                    );
-                  })
-                  }
-
-                </select>
-              </div>
+                {feedbackCategory?.map((ele) => {
+                  return (
+                    <>
+                      <option value={ele?.configurationId}>{ele?.value}</option>
+                    </>
+                  );
+                })}
+              </select>
+              <label for="category">Select the category of your feedback</label>
             </div>
 
-
-            <div className="col-6-area">
-              <div className="form-group">
-                <select
-                  id="accessType"
-                  name="accessType"
-                  className="form-control"
-                  onChange={(e) =>
-                    setFormData({ ...formData, feedbackCategory: e.target.value })
-                  }
-                  required
-                >
-                  <option disabled selected value="">
-                    Select Types
-                  </option>
-
-                  {feedbackCategory?.map((ele) => {
-                    return (
-                      <>
-                        <option value={ele?.configurationId}>
-                          {ele?.value}
-                        </option>
-                      </>
-                    );
-                  })
-                  }
-                </select>
-              </div>
-            </div>
-            <FloatingLabel
-              label="Enter your feedback:"
-              for="validationDefault04"
-              className="form-textA  mb-3"
-            >
-              <Form.Control
-                as="textarea"
+            <div className="form-floating mb-3 mt-3">
+              <input
+                className="form-control"
+                type="textarea"
+                id="message"
                 name="message"
+                maxLength={100}
                 placeholder="Message"
-                id="validationDefault04"
                 onChange={(e) =>
                   setFormData({ ...formData, descriptions: e.target.value })
                 }
                 style={{ height: 100 }}
                 required
               />
-            </FloatingLabel>
+              <label for="fullname">Enter your feedback:</label>
+            </div>
+
             <h5>
-              Please fill your details to respond you to let you know that we
-              have received it.
+              Please provide your details so that we can respond to you and
+              inform you that we have received them.
             </h5>
 
-            <FloatingLabel label="Full Name" className="dense mb-3">
-              <Form.Control
+            <div className="form-floating mb-3 mt-3">
+              <input
+                className="form-control"
                 name="fullName"
+                id="fullName"
+                maxLength={30}
                 type="text"
                 placeholder="FullName"
                 autoComplete="off"
-                id="validationDefault02"
                 onChange={(e) =>
                   setFormData({ ...formData, userName: e.target.value })
                 }
                 required
               />
-            </FloatingLabel>
+              <label for="fullname">Full Name</label>
+            </div>
 
-            <FloatingLabel
-              label="Email address"
-              for="validationDefault01"
-              className="dense has-icon mb-3"
-            >
-              <Form.Control
+            <div class="form-floating mb-3 mt-3">
+              <input
+                className="form-control"
                 name="email"
+                id="email"
                 type="email"
+                pattern="[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,}$"
+                maxLength={45}
                 placeholder="yourName@gmail.com"
-                id="validationDefault01"
                 onChange={(e) =>
                   setFormData({ ...formData, email: e.target.value })
                 }
                 required
               />
-            </FloatingLabel>
+              <label for="email">Email address</label>
+            </div>
 
-            <FloatingLabel label="Mobile No" className="dense mb-3">
-              <Form.Control
+            <div class="form-floating mt-3 mb-3">
+              <input
+                type="tel"
                 name="mobileNumber"
-                type="text"
+                id="mobileNumber"
+                pattern="[0-9]{10}"
                 placeholder="M.No"
+                className="form-control"
                 autoComplete="off"
-                id="validationDefault02"
+                maxLength={12}
                 onChange={(e) =>
                   setFormData({ ...formData, mobile_Number: e.target.value })
                 }
                 required
               />
-            </FloatingLabel>
+              <label for="mobilenumber">Mobile No</label>
+            </div>
 
-            {/* <button
-            color="primary"
-            size="lg"
-            type="button"
-            className=" form-btn d-block m-auto w-100"
-            style={{ height: 40 }}
-          >
-            Send Feedback
-          </button> */}
-
-            <button type="button" className=" btn btn-dark">
+            <button type="reset" onClick={reset} className=" btn btn-dark">
               RESET
             </button>
-            <button type="button" onClick={handlefeedbackData} className=" form-btn btn btn-danger">
-              SUBMIT
+            <button type="submit" className=" form-btn btn btn-danger">
+              SUBMIT{""}
             </button>
           </Form>
         </div>
